a60b8f84b0f1e5c0bfee22e668f2aabda86717d6
Guy <guy.garcia@upr.edu>
2021-01-02 16:08:04 -0500

    Improved script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
-python3 ./tools/menuBuilder.py ./tools/menu.json ./src/menu.hpp
-make
+python3 ./tools/menuBuilder.py ./tools/menu.json ./menu/include/menumod/menu.hpp
+rm -r build
+mkdir build
+cd build
+cmake ..
+make -j2
