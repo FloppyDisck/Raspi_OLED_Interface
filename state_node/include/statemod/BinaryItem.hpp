467594b13715bfa5c2fc2d30055944bd166497e0
Guy <guy.garcia@upr.edu>
2021-01-03 18:58:13 -0500

    Added binaryitem with functor support

---
+++ state_node/include/statemod/BinaryItem.hpp
@@ -0,0 +1,16 @@
+#ifndef BINARYITEM_HPP
+#define BINARYITEM_HPP
+
+#include <functional>
+#include "statemod/StateItem.hpp"
+
+class BinaryItem : public IntegerItem {
+public:
+    BinaryItem(const char* name, Node* previous, std::function<void(bool)> function);
+    Node* input(int input) override;
+    void draw() override;
+protected:
+    std::function<void(bool)> f;
+};
+
+#endif
