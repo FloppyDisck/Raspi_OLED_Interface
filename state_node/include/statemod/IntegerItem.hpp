e37064c9f4640147a8d72c05edfa11dddd6d2798
Guy <guy.garcia@upr.edu>
2021-01-02 19:34:35 -0500

    Functions were not implemented

--- state_node/include/statemod/IntegerItem.hpp
+++ state_node/include/statemod/IntegerItem.hpp
@@ -5,6 +5,11 @@
 class IntegerItem : public StateItem<int> {
 public:
     IntegerItem(const char* name, Node* previous, int minValue, int maxValue, int stepQnty, int initState);
+    Node* input(int input) override;
+    void draw() override;
+
+protected:
+    virtual int update(bool dir) override;
 };
 
 #endif
--- state_node/src/IntegerItem.cpp
+++ state_node/src/IntegerItem.cpp
@@ -2,3 +2,16 @@
 
 IntegerItem::IntegerItem(const char *name, Node *previous, int minValue, int maxValue, int stepQnty, int initState)
         : StateItem(name, previous, minValue, maxValue, stepQnty, initState) {}
+
+Node *IntegerItem::input(int input) {
+    return StateItem::input(input);
+}
+
+void IntegerItem::draw() {
+    StateItem::draw();
+}
+
+int IntegerItem::update(bool dir) {
+    return StateItem::update(dir);
+}
+
