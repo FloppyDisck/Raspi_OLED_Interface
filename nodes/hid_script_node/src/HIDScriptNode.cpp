1a7d04e66f0676b9d577acd847906a5b42f53e30
Guy <guy.garcia@upr.edu>
2021-01-05 22:13:26 -0500

    Changing how keypresses are processed

--- nodes/hid_script_node/src/HIDScriptNode.cpp
+++ nodes/hid_script_node/src/HIDScriptNode.cpp
@@ -188,12 +188,17 @@ Node * HIDScriptNode::input(int input) {
     if (modprobe->getState() == 2) {
         for (char& c : word) {
             std::cout << c << std::endl;
-            std::string keypres = char2HID(c);
-            std::cout << keypres << std::endl;
-            modprobe->HID_OUT << keypres;
-            modprobe->HID_OUT << HID_EMPTY_PACKET;
+            std::string keypress = char2HID(c);
+            std::cout << keypress << std::endl;
+            std::string command = "echo -ne \"" + keypress + "\" > /dev/hidg0";
+            system(command.c_str());
+            //modprobe->HID_OUT << keypress;
+            //modprobe->HID_OUT << HID_EMPTY_PACKET;
         }
     }
+    std::string command = "echo -ne \"" + HID_EMPTY_PACKET + "\" > /dev/hidg0";
+    system(command.c_str());
+
     return previous_;
 }
 
